c2af7a5875aab264865aa2a54d51df283d910931
mstrafiyaislam3-sys <__@github.com>
2025-09-01 19:05:13 +0600

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+<head>
+
+   <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#020e29ff" />
+        <link rel="apple-touch-icon" href="192 favicon.png" />
+
+</head>
+
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+// next.config.js
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = withPWA({
+  reactStrictMode: true,
+});
