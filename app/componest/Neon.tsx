1a2cc0f869b9e2205bf2216989258ca1401c0c7e
mstrafiyaislam3-sys <__@github.com>
2025-09-03 10:47:49 +0600

    Add new feature

--- app/componest/Neon.tsx
+++ app/componest/Neon.tsx
@@ -18,14 +18,14 @@
 "use client";
 import { motion } from "framer-motion";
 
-export default function NeonCard({ title, description }) {
+export default function NeonCard() {
   return (
     <motion.div
       whileHover={{ scale: 1.05, boxShadow: '0 0 20px #00ffff, 0 0 40px #ff00ff' }}
       className="p-6 rounded-2xl bg-gradient-to-r from-neonPink to-neonBlue shadow-neon text-center m-4"
     >
-      <h2 className="text-2xl font-bold mb-2">{title}</h2>
-      <p className="text-sm">{description}</p>
+      <h2 className="text-2xl font-bold mb-2">{'md rashidul'}</h2>
+      <p className="text-sm">{'kamal khan'}</p>
     </motion.div>
   );
 }
