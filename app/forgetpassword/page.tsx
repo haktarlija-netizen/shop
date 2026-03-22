f709212ade025942c4b15cecb0b05761602e9b84
hena <haktarlija@gmail.com>
2026-03-22 13:45:08 +0600

    update version = 0.0.1  myshopings new update 22/3/26

--- app/forgetpassword/page.tsx
+++ app/forgetpassword/page.tsx
@@ -8,7 +8,7 @@ export default function ForgotPasswordPage() {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setLoading(true);
 
